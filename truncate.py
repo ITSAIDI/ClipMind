7d0219663435c3334f89c2866ba8c74d46b8e4e5
ITSAIDI <noureddinesaidi111@gmail.com>
2026-04-15 18:39:13 +0100

    CleanUp

--- truncate.py
+++
@@ -1,7 +0,0 @@
-from moviepy import AudioFileClip
-
-audio = AudioFileClip("audios/01.wav")
-truncated = audio.subclipped(0, 15)
-truncated.write_audiofile("audios/010.wav")
-
-# uv run python truncate.py
